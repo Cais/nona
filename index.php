c4131cc69a257d7bd39602f926049f67ad2d55d4
Edward Caissie <edward.caissie@gmail.com>
2011-12-13 10:23:10 -0500

    Begin version 1.4.1-alpha updates; add primary doc-block to 'index'

--- index.php
+++ index.php
@@ -1,4 +1,46 @@
-<?php get_header(); ?>
+<?php
+/**
+ * NoNa
+ *
+ * Theme Description: Missing the pretty pin-up girl posing in the header, this
+ * grunge based theme originally known as Pinup Meets Grunge beautifully blends
+ * custom background colors.
+ *
+ * @package     NoNa
+ * @since       1.0
+ *
+ * @link        http://buynowshop.com/themes/nona/
+ * @link        https://github.com/Cais/nona/
+ * @link        http://wordpress.org/extend/themes/nona
+ *
+ * @version     1.4.1-alpha
+ * @author      Edward Caissie <edward.caissie@gmail.com>
+ * @copyright   Copyright (c) 2009-2011, Edward Caissie
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License version 2, as published by the
+ * Free Software Foundation.
+ *
+ * You may NOT assume that you can use any other version of the GPL.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to:
+ *
+ *      Free Software Foundation, Inc.
+ *      51 Franklin St, Fifth Floor
+ *      Boston, MA  02110-1301  USA
+ *
+ * The license for this software can also likely be found here:
+ * http://www.gnu.org/licenses/gpl-2.0.html
+ *
+ * Last revised November 6, 2011
+ */
+
+get_header(); ?>
 <div id="main-blog">
     <div id="before-content"></div>
     <div id="content">
