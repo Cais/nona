8800b8e4ebaecf8cef2bd003443062d4bf901509
Edward Caissie <edward.caissie@gmail.com>
2012-04-23 12:34:39 -0400

    Code optimization and quality review

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <?php
 /**
- * Footer Template
+ * Header Template
  *
  * @package     NoNa
  * @since       1.0
