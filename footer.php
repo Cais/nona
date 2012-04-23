5c5c7034dafee6e2e0cb60a1428d88c6e141403d
Edward Caissie <edward.caissie@gmail.com>
2012-04-23 12:10:20 -0400

    Code optimization and quality review

--- footer.php
+++ footer.php
@@ -1,3 +1,18 @@
+<?php
+/**
+ * Footer Template
+ *
+ * @package     NoNa
+ * @since       1.0
+ *
+ * @link        http://buynowshop.com/themes/nona/
+ * @link        https://github.com/Cais/nona/
+ * @link        http://wordpress.org/extend/themes/nona
+ *
+ * @author      Edward Caissie <edward.caissie@gmail.com>
+ * @copyright   Copyright (c) 2009-2012, Edward Caissie
+ */ ?>
+
 <div id="footer">
     <div id="footer-top"></div>
 
@@ -11,20 +26,18 @@
                     <!-- Middle Footer Widget -->
                     <?php nona_login(); ?>
                 </div>
-                <div class="widget-bottom"></div>
-            <?php endif; ?> <!-- end widget zone footer-middle -->
-        </div> <!-- #fw-middle -->
+            <div class="widget-bottom"></div>
+            <?php endif; ?><!-- end widget zone footer-middle -->
+        </div><!-- #fw-middle -->
 
         <div id="fw-left" class="fw-column">
-            <?php if ( dynamic_sidebar( "footer-left" ) ) : else :
-            endif; ?> <!-- end widget zone footer-left -->
-        </div> <!-- #fw-left -->
+            <?php if ( dynamic_sidebar( "footer-left" ) ) : else : endif; ?>
+        </div><!-- #fw-left -->
 
         <div id="fw-right" class="fw-column">
-            <?php if ( dynamic_sidebar( "footer-right" ) ) : else :
-            endif; ?> <!-- end widget zone footer-right -->
-        </div> <!-- #fw-right -->
-    </div> <!-- #footer-widgets -->
+            <?php if ( dynamic_sidebar( "footer-right" ) ) : else : endif; ?>
+        </div><!-- #fw-right -->
+    </div><!-- #footer-widgets -->
     <div id="footer-widgets-below"></div>
 
     <div id="footer-middle">
@@ -32,11 +45,10 @@
             <?php nona_dynamic_copyright();
             nona_theme_version(); ?>
         </p>
-    </div> <!-- #footer-middle -->
+    </div><!-- #footer-middle -->
 
     <div id="footer-bottom"><?php wp_footer(); ?></div>
-</div> <!-- #footer -->
-</div> <!-- #outside -->
-<?php /* Last revised October 24, 2011 v1.4 */ ?>
+</div><!-- #footer -->
+</div><!-- #outside -->
 </body>
 </html>
