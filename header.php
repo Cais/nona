3a24ffbef1f8539a6596e89865bcb728d65cb2a1
Edward Caissie <edward.caissie@gmail.com>
2016-02-01 21:48:11 -0500

    * Update required version to WordPress 4.1 (no sanity check)

--- header.php
+++ header.php
@@ -11,28 +11,12 @@
  *
  * @author      Edward Caissie <edward.caissie@gmail.com>
  * @copyright   Copyright (c) 2009-2016, Edward Caissie
- *
- * @version     1.8
- * @date        March 14, 2013
- * Updated DOCTYPE and other related header elements
- * Code formatting to be more easily read
- * Move Comment Reply script enqueue to 'functions.php'
- *
- * @version     1.9.2
- * @date        December 27, 2014
- * Added check for WordPress 4.1 `add_theme_support( 'title-tag' )` functionality
  */
 ?>
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 	<head>
 		<meta charset="<?php bloginfo( 'charset' ); ?>" />
-
-		<?php /** Check for WordPress 4.1 compatibility */
-		if ( ! function_exists( '_wp_render_title_tag' ) ) { ?>
-			<title><?php wp_title( '|', true, 'right' ); ?></title>
-		<?php } ?>
-
 		<link rel="profile" href="http://gmpg.org/xfn/11" />
 		<link rel="stylesheet" type="text/css" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
 		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
